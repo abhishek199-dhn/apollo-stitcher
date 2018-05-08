451351a3744d69970b13159598400b3c430c8736
abhishek199-dhn <abhishek1992-dhn@gmail.com>
2018-05-08 21:30:44 +0530

    fix(stitcher): added fix for Apollo Server

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ import {mergeSchemas} from "graphql-tools";
 import Logger from "./utils/logger";
 import routes from "./routes";
 import introspection from "./utils/introspection";
+import {ApolloEngine} from "apollo-engine";
 
 const cluster = require("cluster");
 
@@ -20,53 +21,23 @@ const cluster = require("cluster");
         const schemaTypeDefs = await Promise.all(endpoints.map(ep => introspection.getIntrospectSchema(ep)));
         const mergedSchema = mergeSchemas({schemas: schemaTypeDefs});
 
-        if (cluster.isMaster) {
-            const numWorkers = require("os").cpus().length;
+        const app = express();
+        app.set("port", 3000);
 
-            console.log(`Master cluster setting up ${numWorkers} workers...`);
+        app.use(favicon(path.join(__dirname, "../public", "favicon.ico")));
+        app.use(cors("*"));
+        app.use("/", routes);
 
-            for (let i = 0; i < numWorkers; i++) {
-                cluster.fork();
-            }
+        // catch 404 and forward to error handler
+        app.use((req, res, next) => {
+            Logger.error("Not Found 404", req);
+            const err = new Error("Not Found");
+            err.status = 404;
+            next(err);
+        });
 
-            cluster.on("online", (worker) => {
-                console.log(`Worker ${worker.process.pid} is online`);
-            });
-
-            cluster.on("exit", (worker, code, signal) => {
-                console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
-                console.log("Starting a new worker");
-                cluster.fork();
-            });
-        } else {
-
-            const app = express();
-            app.set("port", process.env.PORT || 3000);
-
-            app.use(favicon(path.join(__dirname, "../public", "favicon.ico")));
-            app.use(cors("*"));
-            app.use("/", routes);
-
-            // catch 404 and forward to error handler
-            app.use((req, res, next) => {
-                Logger.error("Not Found 404", req);
-                const err = new Error("Not Found");
-                err.status = 404;
-                next(err);
-            });
-
-            // development error handler will print stacktrace
-            if (app.get("env") === "development") {
-                app.use((err, req, res, next) => {
-                    res.status(err.status || 500);
-                    Logger.error("error", {
-                        message: err.message,
-                        error: err
-                    });
-                });
-            }
-
-            // production error handler no stacktrace leaked to user
+        // development error handler will print stacktrace
+        if (app.get("env") === "development") {
             app.use((err, req, res, next) => {
                 res.status(err.status || 500);
                 Logger.error("error", {
@@ -74,15 +45,24 @@ const cluster = require("cluster");
                     error: err
                 });
             });
-
-            const server = new ApolloServer({typeDefs: mergedSchema});
-            registerServer({server, app});
-
-            // normal ApolloServer listen call but url will contain /graphql
-            server.listen().then(({url}) => {
-                Logger.info("Node app is running on port", app.get("port"));
-            });
         }
+
+        // production error handler no stacktrace leaked to user
+        app.use((err, req, res, next) => {
+            res.status(err.status || 500);
+            Logger.error("error", {
+                message: err.message,
+                error: err
+            });
+        });
+
+         const server = new ApolloServer({app, schema: mergedSchema, engine: true});
+
+         // normal ApolloServer listen call but url will contain /graphql
+         server.listen({port: 3005}).then(({url}) => {
+             Logger.info(`🚀 Server ready at ${url}`);
+         });
+
     } catch (e) {
         Logger.error("error", e);
     }
